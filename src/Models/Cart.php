9ece710a3f1d92bc42243d3cd70d257ce2839073
Oğuzcan Demircan <oguzcandemircan@gmail.com>
2021-08-10 14:44:27 +0300

    add Cart Model

---
+++ src/Models/Cart.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace OguzcanDemircan\LaravelCart\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Cart extends Model
+{
+    use HasFactory;
+
+    protected $casts = [
+        'options' => 'array',
+    ];
+
+    public function buyable()
+    {
+        return $this->morphTo();
+    }
+}
