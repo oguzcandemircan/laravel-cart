ed6956b3ab4a40d858848c2672edbb4909e6b9dd
Oğuzcan Demircan <oguzcandemircan@gmail.com>
2021-08-10 14:43:30 +0300

    add config

--- config/cart.php
+++
@@ -1,5 +0,0 @@
-<?php
-// config for OguzcanDemircan/ClassName
-return [
-
-];
---
+++ config/laravel-cart.php
@@ -0,0 +1,9 @@
+<?php
+// config for OguzcanDemircan/LaravelCart
+return [
+    'session_name' => 'LaravelCart',
+
+    'model_identity_column' => 'id',
+    'model_price_column' => 'price',
+    'model_quantity_column' => 'quantity',
+];
